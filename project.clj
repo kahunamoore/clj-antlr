1562836e6b7400dcb61ab94ec2418131462ae945
Alan Moore <kahunamoore@gmail.com>
2019-01-22 23:55:43 -0800

    Update project.clj to pick up ANTLR4 4.7.2
    Fixes file/path open problem on AWS lambda, see:
    
    https://github.com/antlr/antlr4/pull/2280
    https://github.com/antlr/antlr4/commit/11d601348eda16248e204992cf4cfabb12dc80b4
    
    Needed by an open Lacinia issue:
    
    https://github.com/walmartlabs/lacinia/issues/175#issuecomment-438451105

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.antlr/antlr4-runtime "4.5.3"]
-                 [org.antlr/antlr4 "4.5.3"]]
+                 [org.antlr/antlr4-runtime "4.7.2"]
+                 [org.antlr/antlr4 "4.7.2"]]
   :profiles {:dev {:dependencies
                    [[criterium "0.4.4"]
                     [cheshire "5.6.3"]
